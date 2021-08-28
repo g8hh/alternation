b54a292477ede2eb50014acdc3b9bc960340336b
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-08-28 14:54:58 -0400

    Upgrade System

--- src/game.js
+++ src/game.js
@@ -1,6 +1,23 @@
-let number = 0
+let number = 10
+let gain = 0
+let producerCosts = [10]
+let producerIncreases = [1]
+let producerProductions = [0]
 
 function numberIncrease(i) {
     number += i
     updateHTML()
 }
+function buyProducer(x){
+    let i = x-1
+    if (number >= producerCosts[i]){
+        number -= producerCosts[i]
+        producerCosts[i] *= 1.08
+        producerProductions[i] += producerIncreases[i]
+        gain += producerIncreases[i]
+        updateHTML()
+    }
+}
+window.setInterval(function(){
+    numberIncrease(gain)
+}, 1000);
--- src/update.js
+++ src/update.js
@@ -1,3 +1,6 @@
 function updateHTML(){
-    document.getElementById("numberButton").innerHTML = `You have ${number}`;
+    document.getElementById("numberDisplay").innerHTML = `Your Number is ${number}`
+    document.getElementById("numberGainDisplay").innerHTML = `You are gaining ${gain}/s`
+    document.getElementById("producerOne").innerHTML =
+        `Producer 1<br>Cost: ${producerCosts[0]}<br>Currently Producing ${producerProductions[0]}/s`;
 }
